d38b555813a7eb5661167039e797fabf126a25ea
jangabrielsson <jan@gabrielsson.com>
2020-02-08 12:56:01 +0100

    lib

--- Lib3.0/TestScene1.lua
+++ Lib3.0/TestScene1.lua
@@ -12,25 +12,25 @@
   "stopOnAlarm": false,
   "restart": true,
   "enabled": true,
-  "conditions": {
+  "conditions": "{
     conditions = { 
       { id = 41,
         isTrigger = true,
-        operator = "==",
-        property = "value",
-        type = "device",
+        operator = '==',
+        property = 'value',
+        type = 'device',
         value = false
       }, 
       { isTrigger = true,
-        operator = "matchInterval",
-        property = "cron",
-        type = "date",
+        operator = 'matchInterval',
+        property = 'cron',
+        type = 'date',
         value = {
-          date = { "00", "10", "4", "2", "*", "2020" },
+          date = { '00', '10', '4', '2', '*', '2020' },
           interval =  1200
         }
       } 
-    },
+    }",
     operator = "all"
   }
 }
