9e1e5f741149537eea1255802012da1e8545e271
jangabrielsson <jan@gabrielsson.com>
2020-02-08 13:08:08 +0100

    lib

--- Lib3.0/TestScene1.lua
+++ Lib3.0/TestScene1.lua
@@ -1,36 +1,36 @@
 --[[
 %%LibScene
-{
-  "id": 26,
-  "name": "ttt",
-  "type": "lua",
-  "mode": "manual",
-  "maxRunningInstances": 2,
-  "icon": "scene_block",
-  "hidden": false,
-  "protectedByPin": false,
-  "stopOnAlarm": false,
-  "restart": true,
-  "enabled": true,
-  "conditions": "{
+properties: {
+name: "ttt",
+type: "lua",
+mode: "manual",
+maxRunningInstances: 2,
+icon: "scene_block",
+hidden: false
+protectedByPin: false
+stopOnAlarm: false
+restart: true
+enabled: true
+}
+conditions: {
     conditions = { 
       { id = 41,
         isTrigger = true,
-        operator = '==',
-        property = 'value',
-        type = 'device',
+        operator = "==",
+        property = "value",
+        type = "device",
         value = false
       }, 
       { isTrigger = true,
-        operator = 'matchInterval',
-        property = 'cron',
-        type = 'date',
+        operator = "matchInterval",
+        property = "cron",
+        type = "date",
         value = {
-          date = { '00', '10', '4', '2', '*', '2020' },
+          date = { "00", "10", "4", "2", "*", "2020" },
           interval =  1200
         }
       } 
-    }",
+    },
     operator = "all"
   }
 }
