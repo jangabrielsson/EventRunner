ccf1e3724e96a6e7ecb356dcee87dae5efa01edb
jangabrielsson <jan@gabrielsson.com>
2020-02-17 13:37:26 +0100

    New scene

---
+++ Lib3.0/WaitForSensor_v1.lua
@@ -0,0 +1,55 @@
+--[[
+%%LibScene
+properties: {
+"name": "Wait for sensor example"
+}
+conditions: {
+  conditions = { {
+      id = 32,
+      isTrigger = true,
+      operator = "==",
+      property = "value",
+      type = "device",
+      value = true
+    } },
+  operator = "all"
+}
+--]]
+
+local fd=fibaro.debug; function fibaro.debug(...) fd("",...) end -- Get back to old style debug without useless tag ;-)
+
+local light = 55
+local motion = 32
+
+function loopUntil(fun,cont,secs)
+  local function loop() 
+     if not fun() then 
+         fibaro.setTimeout(1000*secs,loop) 
+     else if cont then cont() end end 
+  end
+  fibaro.setTimeout(1000*secs,loop)
+end
+
+fibaro.call(light,"turnOn")    
+
+local safeTime = 0
+
+loopUntil(
+ 
+ function()                                     -- Loop function
+  if safeTime >= maxTime then return true end                
+  safeTime=safeTime+sleepTime                   -- count up safeTime
+  fibaro.debug("Counting up safeTime ",safeTime,maxTime) 
+  if fibaro.getValue(motion,'value') > '0' then -- motion breached
+     safeTime=0                                 -- reset safeTime
+     fibaro.debug("Reset")
+  end
+ end,
+  
+ function()                                     -- Stuff to do after loop finished
+   fibaro.debug("Turning off")
+   fibaro.call(light,"turnOff")    
+ end,
+  
+ sleepTime                                      -- Time to sleep between loops (in seconds)
+)           
