726390144f4c8649e2f681f6e861f43a19a6683e
jangabrielsson <jan@gabrielsson.com>
2020-02-17 08:07:02 +0100

    New dev

---
+++ Lib3.0/TestDevice1.lua
@@ -0,0 +1,21 @@
+--[[
+%%LibDevice
+properties: {
+"name": "Test device1",
+"type":"com.fibaro.binarySensor",
+"variables":{
+   "myVar":"This is a test"
+   },
+"UI":[
+  {"button":"button1","text":"B1"},
+  [{"button":"button2","text":"B2"},{"button":"button3","text":"B3"}],
+  {"slider":"slider1","text":"","min":0,"max":100},
+  {"label":"label1","text":"L1"}
+  ]
+}
+--]]
+
+function QuickApp:onInit()
+   self:debug("Device1")
+end
+ 
