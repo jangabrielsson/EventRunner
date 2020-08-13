a77426845fd0193342254ae7064ea88c92ab06eb
jangabrielsson <jan@gabrielsson.com>
2020-08-13 20:55:08 +0200

    New arch

--- EventRunner4.lua
+++ EventRunner4.lua
@@ -8,31 +8,39 @@ if dofile and not hc3_emulator then
     --deploy=true,
     --proxy=true,
     --offline=true,
+    debug = {trigger=false},
     UI = {
       {label='ERname',text="..."},
-      {button='debugTriggers', text='Triggers:ON'},
+      {button='debugTrigger', text='Triggers:ON'},
       {button='debugPost', text='Post:ON'},
-      {button='debugRules', text='Rules:ON'},
+      {button='debugRule', text='Rules:ON'},
     }
   }
   dofile("fibaroapiHC3.lua")
 end
+
 if hc3_emulator then hc3_emulator.FILE("EventRunner4Engine.lua","EventRunner") end
 if hc3_emulator then hc3_emulator.FILE("Toolbox/Toolbox_basic.lua","Toolbox") end
 if hc3_emulator then hc3_emulator.FILE("Toolbox/Toolbox_events.lua","Toolbox_events") end
 if hc3_emulator then hc3_emulator.FILE("Toolbox/Toolbox_triggers.lua","Toolbox_triggers") end
 if hc3_emulator then hc3_emulator.FILE("Toolbox/Toolbox_files.lua","Toolbox_files") end
 if hc3_emulator then hc3_emulator.FILE("Toolbox/Toolbox_rpc.lua","Toolbox_rpc") end
+if hc3_emulator then hc3_emulator.FILE("Toolbox/Toolbox_pubsub.lua","Toolbox_pubsub") end
 ----------- Code -----------------------------------------------------------
 
-_debugFlags = {  fcall=true, triggers=true, post = true, rule=true  } 
 
+_debugFlags.trigger = true -- log incoming triggers
+_debugFlags.fcall=true     -- log fibaro.call
+_debugFlags.post = true    -- log internal posts
+_debugFlags.rule=true      -- log rules being invoked (true or false)
+_debugFlags.ruleTrue=true  -- log only rules that are true
+_debugFlags.pubsub=true    -- log only rules that are true
 ------------- Put your rules inside QuickApp:main() -------------------
 
 function QuickApp:main()    -- EventScript version
   local rule = function(...) return self:evalScript(...) end          -- old rule function
   self:enableTriggerType({"device","global-variable","custom-event"}) -- types of events we want
-
+  
   HT = { 
     keyfob = 26, 
     motion= 21,
@@ -43,11 +51,14 @@ function QuickApp:main()    -- EventScript version
   Util.defvars(HT)
   Util.reverseMapDef(HT)
   
-  rule("@@00:00:05 => log(now % 2 == 1 & 'Tick' | 'Tock')")
+  --rule("@@00:00:05 => log(now % 2 == 1 & 'Tick' | 'Tock')")
+  rule("remote(1356,#foo)")
+  rule("wait(5); publish(#foo)")
+  --rule("motion:value => log('Motion:%s',motion:last)")
 
   rule("@{catch,05:00} => Util.checkForUpdates()")
   rule("#File_update{} => log('New file version:%s - %s',env.event.file,env.event.version)")
-  --rule("#File_update{} => Util.updayeFile(env.event.file)")
+  rule("#File_update{} => Util.updateFile(env.event.file)")
   
 --  rule("keyfob:central => log('Key:%s',env.event.value.keyId)")
 --  rule("motion:value => log('Motion:%s',motion:value)")
