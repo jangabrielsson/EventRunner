c51ca55bde8626018ad8f850633e23da44a24e91
jangabrielsson <jan@gabrielsson.com>
2021-10-18 23:38:03 +0200

    Fix UI buttons

--- TQAEexamples/MyQuickApp.lua
+++ TQAEexamples/MyQuickApp.lua
@@ -11,7 +11,11 @@ _=loadfile and loadfile("TQAE.lua"){
 --%%type="com.fibaro.binarySwitch"
 --%%quickVars = {['x'] = 17, ['y'] = 42 }
 --%%noterminate = true
---%%u1={button='b1', text='B1', onReleased='turnOff'}
+--%%u1={button='b1', text='B1', onReleased='turnOn'}
+--%%u2={{button='b2', text='B2', onReleased='turnOff'},{button='b3', text='B3', onReleased='turnOff'}}
+--%%u2={{button='b4', text='B4', onReleased='turnOff'},{button='b5', text='B5', onReleased='turnOff'},{button='b6', text='B6', onReleased='turnOff'},{button='b7', text='B7', onReleased='turnOff'},{button='b8', text='B8', onReleased='turnOff'}}
+--%%u3={label='l1', text='ABCDEFG'}
+--%%u4={slider='s1', onChanged='slider'}
 -- %%proxy=true
 
 function QuickApp:turnOn()
@@ -22,6 +26,10 @@ end
 function QuickApp:turnOff()
   if self.properties.value == true then self:debug("Turn Off") end
   self:updateProperty("value",false)
+end
+
+function QuickApp:slider(ev)
+  self:debug("Slider ",ev.values[1])
 end
 
 function QuickApp:toggle()
