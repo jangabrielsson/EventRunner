953c73d2522ec82093ca6d833d09375e8b05e8b2
jangabrielsson <jan@gabrielsson.com>
2021-11-08 12:06:27 +0100

    removals

--- gggg.lua
+++
@@ -1,5 +0,0 @@
-setTimeout(function()
-    for _,user in ipairs(api.get("/users") or {}) do
-      self:post({type='location', id=user.id, property=user.atHome and 'enter' or 'away'})
-   end
-end,1)
